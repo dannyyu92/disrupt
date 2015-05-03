02d0f0200d4d520724e21a10a80e0572f9731d74
Kevin Gray <Kevin@GoPosse.com>
2015-05-03 03:22:10 -0400

    User models

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,26 @@
 
 u1 = User.create(
   name: "Rich",
-  phone_number: "17329665575"
+  phone_number: "17329665575",
+  pubid: "DUDTI75445ECX6555"
 )
 
 u2 = User.create(
   name: "Kevin",
-  phone_number: "12033395298"
+  phone_number: "12033395298",
+  pubid: "DUHAL68180LFS6953"
 )
 
 u3 = User.create(
   name: "Al",
-  phone_number: "19146181757"
+  phone_number: "19146181757",
+  pubid: "DUVJT93474LOT2472"
+)
+
+u4 = User.create(
+  name: "Danny",
+  phone_number: "13472012818",
+  pubid: "DUVJT93474GUY2472"
 )
 
 p = Project.create(
@@ -30,7 +39,7 @@ t1 = Task.create(
   description: "Make Rails server",
   status: "not_started",
   project_id: p.id,
-  user_id: u1.id
+  user_id: u4.id
 )
 
 t2 = Task.create(
